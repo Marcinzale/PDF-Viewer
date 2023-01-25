d990c6086cde76a15199e1d033127b36cbe35342
Marcin <marcinzale@gmail.com>
2023-01-25 23:50:59 +0100

    Added UI elements

--- app/src/main/java/com/marcinzale/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/marcinzale/pdfviewer/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.marcinzale.pdfviewer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var read_pdf_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        read_pdf_btn = findViewById(R.id.read_pdf_btn)
+        read_pdf_btn.setOnClickListener{
+            val intent = Intent(this@MainActivity, ViewPdfActivity)
+        }
     }
 }
