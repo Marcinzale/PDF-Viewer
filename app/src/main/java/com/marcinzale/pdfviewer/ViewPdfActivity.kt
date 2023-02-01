da0548959fe0acaef6e0061ae052fb368bcc6826
Marcin <marcinzale@gmail.com>
2023-02-01 21:41:06 +0100

    Added functions for select and open pdf.

--- app/src/main/java/com/marcinzale/pdfviewer/ViewPdfActivity.kt
+++ app/src/main/java/com/marcinzale/pdfviewer/ViewPdfActivity.kt
@@ -1,6 +1,8 @@
 package com.marcinzale.pdfviewer
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,8 +10,8 @@ import com.github.barteksc.pdfviewer.PDFView
 
 class ViewPdfActivity : AppCompatActivity() {
 
-    lateinit var pdfView: PDFView
-    private val PDF_SELECTION_CODE = 100
+    private lateinit var pdfView: PDFView
+    val PDF_SELECTION_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +22,25 @@ class ViewPdfActivity : AppCompatActivity() {
     }
 
     private fun selectPdfFromStorage() {
-        Toast.makeText( this@ViewPdfActivity, "Wybierz plik Pdf", Toast.LENGTH_LONG).show()
+        Toast.makeText(this@ViewPdfActivity, "Select PDF file", Toast.LENGTH_SHORT).show()
         val browseStorage = Intent(Intent.ACTION_GET_CONTENT)
         browseStorage.type = "application/pdf"
         browseStorage.addCategory(Intent.CATEGORY_OPENABLE)
-        startActivityForResult(Intent.createChooser(browseStorage, "Wybierz Pdf"), PDF_SELECTION_CODE)
+        startActivityForResult(Intent.createChooser(browseStorage, "Select PDF"), PDF_SELECTION_CODE)
+    }
+
+    fun showPdfFromUri(uri: Uri?) {
+        pdfView.fromUri(uri)
+            .defaultPage(0)
+            .spacing(10)
+            .load()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==PDF_SELECTION_CODE && resultCode==Activity.RESULT_OK && data != null){
+            val selectedPdf = data.data
+            showPdfFromUri(selectedPdf)
+        }
     }
 }
